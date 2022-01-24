5cc5c46e9e87b9c326f24e2bfbd39645116033e4
Filip Jerga <jerga99@gmail.com>
2022-01-24 09:51:04 +0000

    Folder Structure

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
+
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
