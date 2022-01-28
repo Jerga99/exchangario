8c167bb57c0fe749246e0a037e64db915e4340f3
Filip Jerga <jerga99@gmail.com>
2022-01-28 08:20:30 +0000

    Login and Register Page

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,9 +1,82 @@
-
-
 <template>
   <div class="page-wrapper">
-    <div class="container">
-      I am Login page!
+    <div class="container has-text-centered">
+      <div class="column is-4 is-offset-4">
+        <h3 class="title has-text-grey">Login</h3>
+        <p class="subtitle has-text-grey">Please login to proceed.</p>
+        <div class="box">
+          <form>
+            <div class="field">
+              <div class="control">
+                <input
+                  class="input is-large"
+                  type="email"
+                  placeholder="Your Email"
+                  autofocus=""
+                  autocomplete="email">
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                <input
+                  class="input is-large"
+                  type="password"
+                  placeholder="Your Password"
+                  autocomplete="current-password">
+              </div>
+            </div>
+            <button
+              type="button"
+              class="button is-block is-info is-large is-fullwidth"
+            >
+              Sign In
+            </button>
+          </form>
+        </div>
+        <p class="has-text-grey">
+          <a href="#">Sign In With Google</a>&nbsp;
+          <a href="#">Sign Up</a> &nbsp;·&nbsp;
+          <a href="#">Need Help?</a>
+        </p>
+      </div>
     </div>
   </div>
 </template>
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .hero.is-success {
+    background: #F2F6FA;
+  }
+  .hero .nav, .hero.is-success .nav {
+    -webkit-box-shadow: none;
+    box-shadow: none;
+  }
+  .box {
+    margin-top: 1rem;
+  }
+  .avatar {
+    margin-top: -70px;
+    padding-bottom: 20px;
+  }
+  .avatar img {
+    padding: 5px;
+    background: #fff;
+    border-radius: 50%;
+    -webkit-box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
+    box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
+  }
+  input {
+    font-weight: 300;
+  }
+  p {
+    font-weight: 700;
+  }
+  p.subtitle {
+    padding-top: 1rem;
+  }
+</style>
--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,10 +1,102 @@
-
-
-
 <template>
   <div class="page-wrapper">
-    <div class="container">
-      I am Register page!
+    <div class="container has-text-centered">
+      <div class="column is-4 is-offset-4">
+        <h3 class="title has-text-grey">Register</h3>
+        <div class="box">
+          <form>
+            <div class="field">
+              <div class="control">
+                <input
+                  class="input is-large"
+                  type="email"
+                  placeholder="Email"
+                  autocomplete="email">
+                <div
+                  class="form-error">
+                  <div class="help is-danger">Invalid Value</div>
+                </div>
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                <input
+                  class="input is-large"
+                  type="text"
+                  placeholder="Username"
+                >
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                <input
+                  class="input is-large"
+                  type="password"
+                  placeholder="Password"
+                  autocomplete="current-password">
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                <input
+                  class="input is-large"
+                  type="password"
+                  placeholder="Repeat the password"
+                >
+              </div>
+            </div>
+            <button
+              type="button"
+              class="button is-block is-info is-large is-fullwidth">
+              Sign Up
+            </button>
+          </form>
+        </div>
+        <p class="has-text-grey">
+          <a>Sign In With Google</a>&nbsp;
+          <a>Sign Up</a>&nbsp;·&nbsp;
+          <a href="../">Need Help?</a>
+        </p>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+</script>
+<style scoped>
+  /* Left formatting  */
+  /* .form-error {
+    text-align: initial;
+  } */
+  .hero.is-success {
+    background: #F2F6FA;
+  }
+  .hero .nav, .hero.is-success .nav {
+    -webkit-box-shadow: none;
+    box-shadow: none;
+  }
+  .box {
+    margin-top: 1rem;
+  }
+  .avatar {
+    margin-top: -70px;
+    padding-bottom: 20px;
+  }
+  .avatar img {
+    padding: 5px;
+    background: #fff;
+    border-radius: 50%;
+    -webkit-box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
+    box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
+  }
+  input {
+    font-weight: 300;
+  }
+  p {
+    font-weight: 700;
+  }
+  p.subtitle {
+    padding-top: 1rem;
+  }
+</style>
