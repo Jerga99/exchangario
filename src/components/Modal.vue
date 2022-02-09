086eb3c747aceba5d6da7fc4e74f9c74d3e1a9cb
Filip Jerga <jerga99@gmail.com>
2022-02-09 08:49:00 +0000

    Reusable Modal

---
+++ src/components/Modal.vue
@@ -0,0 +1,42 @@
+
+<template>
+  <div>
+    <button
+      @click="isOpen = true"
+      class="button is-block is-success is-light is-fullwidth">Update Info</button>
+    <div :class="['modal', {'is-active': isOpen}]">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Update Data</p>
+          <button
+            @click="isOpen = false"
+            class="delete"
+            aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+          <slot />
+        </section>
+        <footer class="modal-card-foot">
+          <button
+            class="button is-success">
+            Save changes
+          </button>
+          <button @click="isOpen = false" class="button">Cancel</button>
+        </footer>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      isOpen: false
+    }
+  }
+}
+</script>
+
--- src/components/ProfileModal.vue
+++ src/components/ProfileModal.vue
@@ -1,73 +1,52 @@
 <template>
-  <div>
-    <button
-      @click="isOpen = true"
-      class="button is-block is-success is-light is-fullwidth">Update Info</button>
-    <div :class="['modal', {'is-active': isOpen}]">
-      <div class="modal-background"></div>
-      <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">User Profile</p>
-          <button
-            @click="isOpen = false"
-            class="delete"
-            aria-label="close"></button>
-        </header>
-        <section class="modal-card-body">
-          <form>
-            <div class="field">
-              <label class="title">Username</label>
-              <input
-                v-model="userProfile.username"
-                class="input">
-            </div>
-            <div class="field">
-              <label class="title">Avatar</label>
-              <input
-                v-model="userProfile.avatar"
-                class="input">
-            </div>
-            <div class="field">
-              <label class="title">Info about user</label>
-              <input
-                v-model="userProfile.info"
-                class="input">
-            </div>
-            <div class="field">
-              <label class="title">Address</label>
-              <input
-                v-model="userProfile.address"
-                class="input">
-            </div>
-            <div class="field">
-              <label class="title">Country</label>
-              <input
-                v-model="userProfile.country"
-                class="input">
-            </div>
-            <div class="field">
-              <label class="title">Phone</label>
-              <input
-                v-model="userProfile.phone"
-                class="input">
-            </div>
-          </form>
-        </section>
-        <footer class="modal-card-foot">
-          <button
-            @click="updateProfile"
-            class="button is-success">
-            Save changes
-          </button>
-          <button @click="isOpen = false" class="button">Cancel</button>
-        </footer>
+  <exchange-modal>
+    <form>
+      <div class="field">
+        <label class="title">Username</label>
+        <input
+          v-model="userProfile.username"
+          class="input">
       </div>
-    </div>
-  </div>
+      <div class="field">
+        <label class="title">Avatar</label>
+        <input
+          v-model="userProfile.avatar"
+          class="input">
+      </div>
+      <div class="field">
+        <label class="title">Info about user</label>
+        <input
+          v-model="userProfile.info"
+          class="input">
+      </div>
+      <div class="field">
+        <label class="title">Address</label>
+        <input
+          v-model="userProfile.address"
+          class="input">
+      </div>
+      <div class="field">
+        <label class="title">Country</label>
+        <input
+          v-model="userProfile.country"
+          class="input">
+      </div>
+      <div class="field">
+        <label class="title">Phone</label>
+        <input
+          v-model="userProfile.phone"
+          class="input">
+      </div>
+    </form>
+  </exchange-modal>
 </template>
 
 <script>
+  import ExchangeModal from "../components/Modal.vue";
   export default {
+    components: {
+      ExchangeModal
+    },
     props: {
       user: {
         type: Object,
