387721ec1c47a252f19fdbdbc05bd904498db755
Filip Jerga <jerga99@gmail.com>
2022-02-14 09:39:41 +0000

    Opportunity references

--- src/store/modules/opportunity.js
+++ src/store/modules/opportunity.js
@@ -1,11 +1,23 @@
 
+import { db } from "../../db";
+import { doc, Timestamp } from "firebase/firestore";
 
 export default {
   namespaced: true,
   actions: {
     createOpportunity(_, {opportunity, onSuccess}) {
+      opportunity.createdAt = Timestamp.fromDate(new Date());
+
+      opportunity.toExchange = doc(db, "exchanges", opportunity.toExchangeId);
+      opportunity.toUser = doc(db, "users", opportunity.toUserId);
+      opportunity.fromUser = doc(db, "users", opportunity.fromUserId);
+
+      if (opportunity.fromExchangeId) {
+        opportunity.fromExchange = doc(db, "exchanges", opportunity.fromExchangeId)
+      }
 
       console.log(opportunity);
+
       onSuccess();
     }
   }
